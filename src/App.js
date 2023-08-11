5ce0ef5df822f69dba4d0ad7dbfdf2675f59568b
manish785 <kumarachilish1997@gmail.com>
2023-08-11 11:12:18 +0530

    Practiced react's hook(useState)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+// import './App.css';
 
 function App() {
+  const [email, setEmail ] = useState(''); // here, use state hold the intail State
+
+
+  const [name, setName] = useState('');
+
+
+  function handleChange (e) { // this hold the next State
+    setEmail(e.target.value);
+  }
+
+  function handleChangeName (e) {
+    setName(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: 10 }}>
+      <input 
+       value={email}
+       onChange={handleChange}
+       />
+
+       <br></br>
+
+       <input 
+       value={name}
+       onChange={handleChangeName}
+       />
+
+       <p>Email: {email}</p>
+       <p>Name: {name}</p>
     </div>
   );
 }
