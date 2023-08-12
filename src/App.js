73435bb41342805f231dd21ff55af094e0078ceb
manish785 <kumarachilish1997@gmail.com>
2023-08-12 11:25:56 +0530

    used useState hooks

--- src/App.js
+++ src/App.js
@@ -1,40 +1,71 @@
-import logo from './logo.svg';
-import React, {useState} from 'react';
-// import './App.css';
+// import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  const [email, setEmail ] = useState(''); // here, use state hold the intail State
+function App(props) {
+  const [userId, setUserId] = useState('1');
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    const url = `https://jsonplaceholder.typicode.com/posts?userId=${userId}`;
 
-  const [name, setName] = useState('');
-
-
-  function handleChange (e) { // this hold the next State
-    setEmail(e.target.value);
-  }
-
-  function handleChangeName (e) {
-    setName(e.target.value);
-  }
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Data', data);
+        setData(data);
+      });
+  }, [userId]);
 
   return (
-    <div className="App" style={{ padding: 10 }}>
-      <input 
-       value={email}
-       onChange={handleChange}
-       />
-
-       <br></br>
-
-       <input 
-       value={name}
-       onChange={handleChangeName}
-       />
-
-       <p>Email: {email}</p>
-       <p>Name: {name}</p>
+    <div className="App" style={{ paddingLeft: 10 }}>
+      <h1>App</h1>
+      <button onClick={() => setUserId('2')}>Change user id to 2</button>
+      {data.map((post) => (
+        <div key={post.id}> {/* Add a unique key */}
+          <p>{post.title}</p>
+        </div>
+      ))}
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+// function App() {
+//   const [email, setEmail ] = useState(''); // here, use state hold the intail State
+
+
+//   const [name, setName] = useState('');
+
+
+//   function handleChange (e) { // this hold the next State
+//     setEmail(e.target.value);
+//   }
+
+//   function handleChangeName (e) {
+//     setName(e.target.value);
+//   }
+
+//   return (
+//     <div className="App" style={{ padding: 10 }}>
+//       <input 
+//        value={email}
+//        onChange={handleChange}
+//        />
+
+//        <br></br>
+
+//        <input 
+//        value={name}
+//        onChange={handleChangeName}
+//        />
+
+//        <p>Email: {email}</p>
+//        <p>Name: {name}</p>
+//     </div>
+//   );
+// }
